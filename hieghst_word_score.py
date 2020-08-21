4101e60e26fc1aaed6ba6a5eb6be07c98b9ecad9
renatkg20 <renatkg2005@gmail.com>
2020-08-21 17:45:55 +0600

    task from codewar

---
+++ hieghst_word_score.py
@@ -0,0 +1,10 @@
+def high(x):
+    # Code here
+    t = [list(i) for i in x.split()]
+    print(t)
+    q = "".join(t)
+    print(q)
+    dict1 = "abcdefghijklmnopqrstuvwxyz"
+
+
+print(high("man i need a taxi up to ubud"))
